97c82b7bf14c6a054b12caefc113ff3c3351642f
froggyflex <42496199+froggyflex@users.noreply.github.com>
2020-03-01 18:57:49 +0100

    Update connect.php

--- app/src/Sorgente Server/connect.php
+++ app/src/Sorgente Server/connect.php
@@ -1,7 +1,7 @@
 <?php
 	
-	$username = "postgres";
-	$password = "f@n357159";
+	$username = "********";
+	$password = "********";
 	$db       = "postgres";
 
 	$db_connection = pg_connect("host=localhost dbname=".$db." user=".$username." password=".$password);
